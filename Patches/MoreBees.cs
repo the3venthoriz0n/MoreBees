092e8eac76b4c5f034e0930502b40fb5b3bcf2ca
the3venthoriz0n <indrewjones@gmail.com>
2023-12-17 00:31:55 -0800

    Needed to add annotation for MoreBees Class

--- Patches/MoreBees.cs
+++ Patches/MoreBees.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 namespace MoreBees.Patches
 {
+    [HarmonyPatch]
     public class MoreBees
     {
         private static FieldInfo currentLevelField;
@@ -13,6 +14,29 @@ namespace MoreBees.Patches
         {
             // Initialize currentLevelField in the static constructor
             currentLevelField = AccessTools.Field(typeof(RoundManager), "currentLevel");
+        }
+
+
+        [HarmonyPatch(typeof(PlayerControllerB), "Update")]
+        [HarmonyPostfix]
+        static void MoreSprint(ref float ___sprintMeter)
+        {
+            ___sprintMeter = 1f;
+
+        }
+
+
+        [HarmonyPatch(typeof(DepositItemsDesk), "SetCompanyMood")]
+        [HarmonyPostfix]
+        static void MoreTentacles(CompanyMood mood, ref CompanyMood ___currentMood, ref float ___noiseBehindWallVolume)
+        {
+            ___currentMood.desiresSilence = true;
+            ___currentMood.sensitivity = 10f;
+            ___currentMood.irritability = 10f;
+            ___currentMood.judgementSpeed = 10f;
+            ___currentMood.timeToWaitBeforeGrabbingItem = 1f;
+            ___currentMood.startingPatience = 0f;
+            ___noiseBehindWallVolume = 10f;
         }
 
 
@@ -105,26 +129,6 @@ namespace MoreBees.Patches
                     }
                 }
             }
-        }
-
-        [HarmonyPatch(typeof(PlayerControllerB), "Update")]
-        static void PostFix(ref float ___sprintMeter)
-        {
-            ___sprintMeter = 1f;
-
-        }
-
-
-        [HarmonyPatch(typeof(DepositItemsDesk), "SetCompanyMood")]
-        static void PostFix(CompanyMood mood, ref CompanyMood ___currentMood, ref float ___noiseBehindWallVolume)
-        {
-            ___currentMood.desiresSilence = true;
-            ___currentMood.sensitivity = 10f;
-            ___currentMood.irritability = 10f;
-            ___currentMood.judgementSpeed = 10f;
-            ___currentMood.timeToWaitBeforeGrabbingItem = 1f;
-            ___currentMood.startingPatience = 0f;
-            ___noiseBehindWallVolume = 10f;
         }
 
     }
